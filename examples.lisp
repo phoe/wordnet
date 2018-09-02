4beff3e788d65ef8bd64fca6c9dcd388590e2fec
Michał "phoe" Herda <phoe@disroot.org>
2018-09-02 18:40:37 +0200

    Update them some more

--- examples.lisp
+++ examples.lisp
@@ -2,20 +2,20 @@
 
 (defun get-state-names ()
   (let ((states nil)
-        (pointers (wn:wordnet-pointers
-                   (car (wn:index-entry-synsets
-                         (wn:cached-index-lookup "american_state" :noun))))))
+        (pointers (wordnet-pointers
+                   (car (index-entry-synsets
+                         (cached-index-lookup "american_state" :noun))))))
     (dolist (p pointers)
-      (when (eq (wn:wordnet-pointer-type p) :hyponym)
-        (push (mapcar #'car (wn:synset-words (wn:wordnet-pointer-to-synset p)))
+      (when (eq (wordnet-pointer-type p) :hyponym)
+        (push (mapcar #'car (synset-words (wordnet-pointer-to-synset p)))
               states)))
     states))
 
 (defun synsets-containing-words (part-of-speech words)
   (reduce #'intersection
           (mapcar #'(lambda (word)
-                      (wn:index-entry-synsets
-                       (wn:cached-index-lookup word part-of-speech)))
+                      (index-entry-synsets
+                       (cached-index-lookup word part-of-speech)))
                   words)))
 
 (defun get-synonyms (words part-of-speech)
@@ -27,31 +27,31 @@
 
 (defun get-antonyms (word part-of-speech)
   (let ((antonyms nil)
-        (synsets (wn:index-entry-synsets
-                  (wn:cached-index-lookup word part-of-speech))))
+        (synsets (index-entry-synsets
+                  (cached-index-lookup word part-of-speech))))
     (dolist (s synsets)
-      (dolist (p (wn:wordnet-pointers s))
-        (when (eq (wn:wordnet-pointer-type p) :antonym)
-          (let ((from (wn:wordnet-pointer-from-word p))
-                (to (wn:wordnet-pointer-to-word p)))
+      (dolist (p (wordnet-pointers s))
+        (when (eq (wordnet-pointer-type p) :antonym)
+          (let ((from (wordnet-pointer-from-word p))
+                (to (wordnet-pointer-to-word p)))
             (when (or (typep from 'wn:wordnet-synset-entry)
                       (and (stringp (car from))
                            (string-equal (car from) word)))
               (if (typep to 'wn:wordnet-synset-entry)
-                  (dolist (w (wn:synset-words to))
+                  (dolist (w (synset-words to))
                     (pushnew (car w) antonyms :test #'string-equal))
                   (pushnew (car to) antonyms :test #'string-equal)))))))
     antonyms))
 
 (defun find-synset-with-sense (part-of-speech word like-word)
-  (let ((word-synsets (wn:index-entry-synsets
-                       (wn:cached-index-lookup word part-of-speech)))
-        (like-word-synsets (wn:index-entry-synsets
-                            (wn:cached-index-lookup like-word part-of-speech)))
+  (let ((word-synsets (index-entry-synsets
+                       (cached-index-lookup word part-of-speech)))
+        (like-word-synsets (index-entry-synsets
+                            (cached-index-lookup like-word part-of-speech)))
         (found nil))
     (dolist (like-synset like-word-synsets)
       (dolist (synset word-synsets)
-        (let ((c (wn::commonality :hypernym synset like-synset)))
+        (let ((c (commonality :hypernym synset like-synset)))
           (when c
             (push c found)))))
     (sort (pairlis (mapcar #'(lambda (f) (apply #'+ (mapcar #'cdr (cdr f)))) found)
